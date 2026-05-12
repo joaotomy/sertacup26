040fa08afee049d848cde518278e273c575fc351
João Tomás <jtomas@sendys.local>
2026-05-12 16:08:26 +0100

    second commit

---
+++ sertacup26-main/api/server.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const sql = require('mssql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const config = {
+  user: 'jtomas',
+  password: 'jtomas',
+  server: 'JAIME',       
+  port: 1433,
+  database: 'sertacup26',
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
+};
+
+sql.connect(config).then(() => {
+  console.log('DB connected');
+  app.listen(3000, () => console.log('API running'));
+}).catch(err => console.error('DB connection failed:', err));
+
+
+app.get('/teams', async (req, res) => {
+  try {
+    const result = await sql.query`SELECT * FROM equipa`;
+    res.json(result.recordset);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+
+app.get('/games', async (req, res) => {
+  try {
+
+    const request = new sql.Request();
+
+    let query = `
+      SELECT *
+      FROM jogo
+      WHERE 1=1
+    `;
+    
+    if (req.query.chave) {
+      query += ` AND chave = @chave`;
+      request.input('chave', sql.VarChar, req.query.chave);
+    }
+
+    if (req.query.equipa1) {
+      query += ` AND equipa1 = @equipa1`;
+      request.input('equipa1', sql.Int, req.query.equipa1);
+    }
+
+    if (req.query.equipa2) {
+      query += ` AND equipa2 = @equipa2`;
+      request.input('equipa2', sql.Int, req.query.equipa2);
+    }
+
+    if (req.query.campo) {
+      query += ` AND campo = @campo`;
+      request.input('campo', sql.Int, req.query.campo);
+    }
+
+    if (req.query.grupo) {
+      query += ` AND grupo = @grupo`;
+      request.input('grupo', sql.Int, req.query.grupo);
+    }
+
+    if (req.query.fase) {
+      query += ` AND fase = @fase`;
+      request.input('fase', sql.Int, req.query.fase);
+    }
+
+    if (req.query.liga) {
+      query += ` AND liga = @liga`;
+      request.input('liga', sql.Int, req.query.liga);
+    }
+
+    if (req.query.comecado) {
+      query += ` AND começado = @comecado`;
+      request.input('comecado', sql.Bit, req.query.comecado);
+    }
+
+    if (req.query.terminado) {
+      query += ` AND terminado = @terminado`;
+      request.input('terminado', sql.Bit, req.query.terminado);
+    }
+
+    if (req.query.jogo_grupo) {
+      query += ` AND jogo_grupo = @jogo_grupo`;
+      request.input('jogo_grupo', sql.Bit, req.query.jogo_grupo);
+    }
+
+    if (req.query.data) {
+      query += `
+        AND CAST(hora_prevista AS DATE) = @data
+      `;
+      request.input('data', sql.Date, req.query.data);
+    }
+
+    const result = await request.query(query);
+
+    res.json(result.recordset);
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
