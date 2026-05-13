37b9228731120334bfc82fe99e4b2c8bd46a1d4e
João Tomás <jtomas@sendys.local>
2026-05-13 09:54:35 +0100

    add smthing

---
+++ sertacup26-main/sertacup26-site/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+export default function HomePage() {
+  return (
+    <>
+      <section className="hero">
+        <div className="hero-overlay"></div>
+
+        <div className="grid-container">
+          <div className="col-span-4 col-start-1 hero-cup-wrapper">
+            <img
+              src="/images/cup test.png"
+              alt="Taça Sertã Cup"
+              className="hero-cup-img"
+            />
+          </div>
+
+          <div className="col-span-6 col-start-6 md:col-span-5 md:col-start-4 sm:col-span-4 sm:col-start-1 hero-text">
+            <div className="hero-title">IV SERTÃ CUP</div>
+
+            <div className="hero-subtitle">28/29 JUNHO</div>
+
+            <div className="cta-wrapper">
+              <a className="cta" href="/torneio?tab=calendario">
+                EM DIRETO
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
