4d0bcd5ea7d5611b239b4f827c535f0681f25bc1
Joao Tomas <jaftomas@gmail.com>
2026-06-20 21:41:32 +0100

    alteraçoes 1 dia fem

--- sertacup26-main/sertacup26-site/src/pages/Torneio.jsx
+++ sertacup26-main/sertacup26-site/src/pages/Torneio.jsx
@@ -117,7 +117,7 @@ const allLeagues = [
 function getPlacementRange(totalPlacements, boxCount, boxIndex) {
   countGetPlacement ++;
 
-  if (countGetPlacement > 24)
+  if (countGetPlacement > 8)
     return `5º–8º LUGAR`;
 
   if (countGetPlacement == 46)
