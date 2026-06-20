648fd5d4eac3355a159e8cd68b42e23f157ed19c
Joao Tomas <jaftomas@gmail.com>
2026-06-20 18:27:21 +0100

    alteraçoes 1 dia fem

--- sertacup26-main/sertacup26-site/src/components/Layout.jsx
+++ sertacup26-main/sertacup26-site/src/components/Layout.jsx
@@ -21,7 +21,7 @@ export default function Layout() {
               <div className="dropdown-content">
                 <Link to="/torneio?tab=calendario">Calendário</Link>
                 <Link to="/torneio?tab=grupos">Grupos</Link>
-                <Link className="disabled-link" to="/torneio?tab=fases-finais">Fases Finais</Link>
+                <span className="disabled-link">Fases Finais</span>
               </div>
             </div>
           </div>
