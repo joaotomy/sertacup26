48f14b15c7968418d093ad3ad64dc91bfb3bfc3e
Joao Tomas <jaftomas@gmail.com>
2026-06-25 10:51:53 +0100

    logo lagartos

--- sertacup26-main/sertacup26-site/src/components/Layout.jsx
+++ sertacup26-main/sertacup26-site/src/components/Layout.jsx
@@ -71,7 +71,7 @@ export default function Layout() {
               <div className="mobile-submenu">
                 <Link to="/torneio?tab=calendario">Calendário</Link>
                 <Link to="/torneio?tab=grupos">Grupos</Link>
-                <Link to="/torneio?tab=fases-finais">Fases Finais</Link>
+                {/* <Link to="/torneio?tab=fases-finais">Fases Finais</Link> */}
               </div>
             )}
           </div>
